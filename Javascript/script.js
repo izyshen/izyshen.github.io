59e677394d97774edba1cba65a77b3f3bf7d53fa
Iris Shen <ishen.zy@gmail.com>
2016-09-30 23:56:14 -0400

    Javascript functions

---
+++ Javascript/script.js
@@ -0,0 +1,10 @@
+function introduction() 
+	{var surname = prompt('Greetings friend, may I enquire as to your surname?');
+	if (surname != null {
+		document.getElementById("demo").innerHTML = 
+		"Hello" + surname + "How are you today?";
+	}
+}
+
+
+	
