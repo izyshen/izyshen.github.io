76620d93b88f5e9cb700ec9303a5d52a7cf96f9c
Iris Shen <ishen.zy@gmail.com>
2016-10-02 22:47:03 -0400

    adding progress images w/ js

--- Javascript/script.js
+++ Javascript/script.js
@@ -7,4 +7,12 @@ function introduction()
 }
 	
 
-	
+function incomplete {
+	<button type="button" onclick="document.getElementById('incomplete').style.display='block'"/>
+}	
+
+function writing {
+	document.getElementById("incomplete").innerHTML = "Test"; 
+	document.getElementById("incomplete").innerHTML = "test";
+	// figure out how to get multiple images to come up
+}
