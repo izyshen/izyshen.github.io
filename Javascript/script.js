575b4d7b7e7f18c29da7fd66035fefc679acd1de
Iris Shen <ishen.zy@gmail.com>
2016-10-03 00:46:51 -0400

    js to add progress pictures and flip through

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,7 +1,7 @@
-function introduction() 
+function introduction
 	{var surname = prompt('Greetings friend, may I enquire as to your surname?');
 	if (surname != null {
-		document.getElementById("demo").innerHTML = 
+		document.getElementById("introduction").innerHTML = 
 		"Hello" + surname + "How are you today?";
 	}
 }
@@ -16,3 +16,6 @@ function writing {
 	document.getElementById("incomplete").innerHTML = "test";
 	// figure out how to get multiple images to come up
 }
+
+var art = {lastName: "surname"};
+document.getElementById("test").innerHTML = "test";
