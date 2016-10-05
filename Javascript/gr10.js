4ac94a03ea14bb221e32b199c7207b54507ab4da
Iris Shen <ishen.zy@gmail.com>
2016-10-05 11:57:32 -0400

    added gr 11 pg, add new art pieces/carousel

--- Javascript/gr10.js
+++ Javascript/gr10.js
@@ -1,12 +1,9 @@
-var text = '{"title": "Title: A Home of Magic", 
-"medium": "Medium : pencil crayon", 
-"inspiration": "Inspiration:insert words here", 
-"notes": "Notes from the author:more words here"}'
+	var text = '{"title": "Title: A Home of Magic", "medium": "Medium: pencil crayon", "inspiration": "Inspiration: Entry for an art contest", "notes": "Notes from the artist: I started getting into reading about fantasy books about this time and I really liked dragons. No message here except I wanted to draw something pretty and colourful".}'
 
-var obj = JSON.parse(text);
+			var obj = JSON.parse(text);
 
-document.getElementById("words").innerHTML = 
-obj.title + "<br>" +
-obj.medium + "<br>" + 
-obj.inspiration + "<br>" +
-obj.notes;
+			document.getElementById("words").innerHTML = 
+			obj.title + "<br>" +
+			obj.medium + "<br>" + 
+			obj.inspiration + "<br>" +
+			obj.notes;
