cf755f7a20ebb47e530c20e69834dff1cea4deb1
Iris Shen <ishen.zy@gmail.com>
2016-10-09 23:45:56 -0400

    data storage

---
+++ Javascript/master.js
@@ -0,0 +1,27 @@
+var gr11 = {
+	"title" : "Bouquet of Joy", 
+	"focus" : "the details on flowers", 
+	"medium" : "water colour", 
+	"notes" : "One of my favourite things to draw are flowers because every fold creates so many dynamic shades of red in a rose."
+}, 
+{
+	"title" : "Pinball Party", 
+	"medium" : "clay, pencil crayon, construction paper, wood, paint", 
+	"inspiration" : "Charles Bell - an incredible still life painter with extreme care for detail and love for bright colours.", 
+	"notes" : "The background of the board contains facts about Charles Bell. I always tried to add a little more in my art pieces made in school. The addition of wood was unique amonst my classmates; they stuck to using only clay. Believe me when I say I was extremely excited to see this design come to life. "
+}, 
+{
+	"title" : ""
+	"medium" : "pencil crayons, oil pastel, charcoal"
+	"inspiration" : "Inspiration: The impending evil that seems to come with life"
+	"notes" : "It's peculiar how joyful and openminded children are when they are young but as we grow, we seem to lose that. That's what this drawing aims to capture: the brightness and optimism of children being corrupted. I don't mean this to be pessimistic but it is how life seems to be. That's why we should all tap into our inner child and see the world from their point of view from time to time."
+}
+
+var gr10 = {
+	"title" : "A Home of Magic", 
+	"medium" : "pencil crayon", 
+	"notes" : "Entry to an art contest. I started getting into reading about fantasy books about this time and I really liked dragons. No message here except I wanted to draw something pretty and colourful. "
+}
+
+
+
