b74ba7cfcaccb5460bc84af745d23a23d922633f
Iris Shen <ishen.zy@gmail.com>
2016-10-12 14:21:28 -0400

    fix up design

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,11 +1,3 @@
-function introduction
-	{var surname = prompt('Greetings friend, may I enquire as to your surname?');
-	if (surname != null {
-		document.getElementById("introduction").innerHTML = 
-		"Hello" + surname + "How are you today?";
-	}
-}
-
 function incomplete {
 	<button type="button" onclick="document.getElementById('incomplete').style.display='block'"/>
 }	
@@ -15,6 +7,3 @@ function writing {
 	document.getElementById("incomplete").innerHTML = "test";
 	// figure out how to get multiple images to come up
 }
-
-var art = {lastName: "surname"};
-document.getElementById("test").innerHTML = "test";
