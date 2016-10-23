ee63de266e6f821c36e4f048b34e1ed2b18847d7
Iris Shen <ishen.zy@gmail.com>
2016-10-23 00:49:20 -0400

    multiple carousels on one pg, need size adjustments

--- Javascript/gr11.js
+++ Javascript/gr11.js
@@ -4,7 +4,76 @@ function button2() {
 	document.getElementById("incomplete").style.display='none';}
 
 carousel = (function(){
-  var box = document.querySelector('.carouselbox');
+  var box = document.querySelector('.carouselbox1');
+  var next = box.querySelector('.next');
+  var prev = box.querySelector('.prev');
+  var items = box.querySelectorAll('.content li');
+  var counter = 0;
+  var amount = items.length;
+  var current = items[0];
+  box.classList.add('active');
+  function navigate(direction) {
+    current.classList.remove('current');
+    counter = counter + direction;
+    if (direction === -1 && 
+        counter < 0) { 
+      counter = amount - 1; 
+    }
+    if (direction === 1 && 
+        !items[counter]) { 
+      counter = 0;
+    }
+    current = items[counter];
+    current.classList.add('current');
+  }
+  next.addEventListener('click', function(ev) {
+    navigate(1);
+  });
+  prev.addEventListener('click', function(ev) {
+    navigate(-1);
+  });
+  navigate(0);
+})();
+
+function button1() {
+  document.getElementById("incomplete").style.display='block';}
+function button2() {
+  document.getElementById("incomplete").style.display='none';}
+
+carousel = (function(){
+  var box = document.querySelector('.carouselbox2');
+  var next = box.querySelector('.next');
+  var prev = box.querySelector('.prev');
+  var items = box.querySelectorAll('.content li');
+  var counter = 0;
+  var amount = items.length;
+  var current = items[0];
+  box.classList.add('active');
+  function navigate(direction) {
+    current.classList.remove('current');
+    counter = counter + direction;
+    if (direction === -1 && 
+        counter < 0) { 
+      counter = amount - 1; 
+    }
+    if (direction === 1 && 
+        !items[counter]) { 
+      counter = 0;
+    }
+    current = items[counter];
+    current.classList.add('current');
+  }
+  next.addEventListener('click', function(ev) {
+    navigate(1);
+  });
+  prev.addEventListener('click', function(ev) {
+    navigate(-1);
+  });
+  navigate(0);
+})();
+
+carousel = (function(){
+  var box = document.querySelector('.carouselbox3');
   var next = box.querySelector('.next');
   var prev = box.querySelector('.prev');
   var items = box.querySelectorAll('.content li');
