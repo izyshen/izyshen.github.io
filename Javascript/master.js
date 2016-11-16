db886df7fd1d19dd0ae509a0c04420ab722304a4
Iris Shen <ishen.zy@gmail.com>
2016-11-16 01:37:30 -0500

    JSON experiments

--- Javascript/master.js
+++ Javascript/master.js
@@ -1,42 +1,70 @@
-myFunction([
-{
-	"flower" : {
-		"title" : "Bouquet of Joy", 
-		"focus" : "the details on flowers", 
-		"medium" : "water colour", 
-		"notes" : "One of my favourite things to draw are flowers because every fold creates so many dynamic shades of red in a rose."
-	}, 
-	"pinball" : {
-		"title" : "Pinball Party", 
-		"medium" : "clay, pencil crayon, construction paper, wood, paint", 
-		"inspiration" : "Charles Bell - an incredible still life painter with extreme care for detail and love for bright colours.", 
-		"notes" : "The background of the board contains facts about Charles Bell. I always tried to add a little more in my art pieces made in school. The addition of wood was unique amonst my classmates; they stuck to using only clay. Believe me when I say I was extremely excited to see this design come to life. "
-	}, 
-	"child" : {
-		"title" : "Childhood"
-		"medium" : "pencil crayons, oil pastel, charcoal"
-		"inspiration" : "Inspiration: The impending evil that seems to come with life"
-		"notes" : "It's peculiar how joyful and openminded children are when they are young but as we grow, we seem to lose that. That's what this drawing aims to capture: the brightness and optimism of children being corrupted. I don't mean this to be pessimistic but it is how life seems to be. That's why we should all tap into our inner child and see the world from their point of view from time to time."
+// textdisplay([
+// {
+// 	"flower" : {
+// 		"title" : "Bouquet of Joy", 
+// 		"focus" : "the details on flowers", 
+// 		"medium" : "water colour", 
+// 		"notes" : "One of my favourite things to draw are flowers because every fold creates so many dynamic shades of red in a rose."
+// 	}, 
+// 	"pinball" : {
+// 		"title" : "Pinball Party", 
+// 		"medium" : "clay, pencil crayon, construction paper, wood, paint", 
+// 		"inspiration" : "Charles Bell - an incredible still life painter with extreme care for detail and love for bright colours.", 
+// 		"notes" : "The background of the board contains facts about Charles Bell. I always tried to add a little more in my art pieces made in school. The addition of wood was unique amonst my classmates; they stuck to using only clay. Believe me when I say I was extremely excited to see this design come to life. "
+// 	}, 
+// 	"child" : {
+// 		"title" : "Childhood"
+// 		"medium" : "pencil crayons, oil pastel, charcoal"
+// 		"inspiration" : "Inspiration: The impending evil that seems to come with life"
+// 		"notes" : "It's peculiar how joyful and openminded children are when they are young but as we grow, we seem to lose that. That's what this drawing aims to capture: the brightness and optimism of children being corrupted. I don't mean this to be pessimistic but it is how life seems to be. That's why we should all tap into our inner child and see the world from their point of view from time to time."
+// 	}
+// }
+// ]);
+
+// textdisplay([
+// {
+// 	"general" : { 
+// 		"title" : "Title: ",
+// 		"focus" : "Focus: ",
+// 		"medium" : "Medium: ",
+// 		"inspiration" : "Inspiration: ",
+// 		"notes" : "Notes from the Author: "
+// 		},
+// 	"flower" : {
+// 		"title" : "Bouquet of Joy",
+// 		"focus" : "the details on flowers",
+// 		"medium" : "watercolour",
+// 		"notes" : "One of my favourite things to draw are flowers because every fold creates so many dynamic shades of red in a rose."
+// 		}, 
+// 	"pinball" : {
+// 		"title" : "Pinball Party",
+// 		"medium" : "clay, pencil crayon, construction paper, wood, paint",
+// 		"inspiration" : "Charles Bell - an incredible still life painter with extreme care for detail and love for bright colours.",
+// 		"notes" : "The background of the board contains facts about Charles Bell. I always tried to add a little more in my art pieces made in school. The addition of wood was unique amonst my classmates; they stuck to using only clay. Believe me when I say I was extremely excited to see this design come to life."
+// 		},
+// 	"child" : {
+// 		"title" : "Childhood",
+// 		"medium" : "pencil crayons, oil pastel, charcoal",
+// 		"inspiration" : "The impending evil that seems to come with life",
+// 		"notes" : "It is peculiar how joyful and openminded children are when they are young but as we grow, we seem to lose that. That is what this drawing aims to capture: the brightness and optimism of children being corrupted. Not meant to be pessimistic but it is how life seems to be. That is why we should all tap into our inner child and see the world from their point of view from time to time."
+// 		}
+// }]);
+
+var xmlhttp = new XMLHttpRequest();
+var url = "Javascript/text.txt";
+
+xmlhttp.onreadystatechange = function() {
+	if (this.readyState == 4 && this.status == 200) {
+		var myArr = JSON.parse(this.responseText);
+		textdisplay(descstorage);
 	}
+};
+
+function textdisplay(arr) {
+	var out = "";
+	var i;
+	for(i = 0; i < arr.length; i++) {
+		out += arr[i].title + arr[i].medium + '<br>';
+	}
+	document.getElementById("desc").innerHTML = out;
 }
-]);
-
-// var gr10 = {
-// 	"title" : "A Home of Magic", 
-// 	"medium" : "pencil crayon", 
-// 	"notes" : "Entry to an art contest. I started getting into reading about fantasy books about this time and I really liked dragons. No message here except I wanted to draw something pretty and colourful. "
-// }
-// 		document.write(gr11.flower.title);
-
-
-
-// 			<div id="title1"></div>
-// 			<script src="Javascript/master.js"></script>
-// 			function title() {
-// 	document.write('Title: ' gr11.flower.title);
-// 	document.getElementById("title1").innerHTML
-// }
-
-// var obj = JSON.parse(gr11);
-// document.getElementById("title1").innerHTML = 
-// document.write(gr11.flower.title);
--- Javascript/test.js
+++ Javascript/test.js
@@ -13,40 +13,82 @@
 // }
 // ]; 
 
-textdisplay([
-{
-	"title" : "Flowers",
-	"medium" : "Watercolour"
-}, 
-{
-	"title" : "Pinball", 
-	"medium" : "Wood, clay, paint, construction paper"
-}, 
-{
-	"title" : "Childhood", 
-	"medium" : "Oil Pastel"
-}
-]); 
+// textdisplay([
+// {
+// 	"title" : "Flowers",
+// 	"medium" : "Watercolour"
+// }, 
+// {
+// 	"title" : "Pinball", 
+// 	"medium" : "Wood, clay, paint, construction paper"
+// }, 
+// {
+// 	"title" : "Childhood", 
+// 	"medium" : "Oil Pastel"
+// }
+// ]); 
+
+
 
 var xmlhttp = new XMLHttpRequest();
-var url = "text.txt";
+var url = "Javascript/text.txt";
 
 xmlhttp.onreadystatechange = function() {
 	if (this.readyState == 4 && this.status == 200) {
 		var myArr = JSON.parse(this.responseText);
-		textdisplay(descstorage);
+		textdisplay(myArr);
 	}
 };
-xmlhttp.open("GET", url, true);
-xmlhttp.send();
-
-// textdisplay(descstorage);
+// 		textdisplay([
+// {
+// 	"title" : "Flowers",
+// 	"medium" : "Watercolour"
+// }, 
+// {
+// 	"title" : "Pinball", 
+// 	"medium" : "Wood, clay, paint, construction paper"
+// }, 
+// {
+// 	"title" : "Childhood", 
+// 	"medium" : "Oil Pastel"
+// }
+// ]); 
+// 		textdisplay([
+// {
+// 	"general" : { 
+// 		"title" : "Title: ",
+// 		"focus" : "Focus: ",
+// 		"medium" : "Medium: ",
+// 		"inspiration" : "Inspiration: ",
+// 		"notes" : "Notes from the Author: "
+// 		},
+// 	"flower" : {
+// 		"title" : "Bouquet of Joy",
+// 		"focus" : "the details on flowers",
+// 		"medium" : "watercolour",
+// 		"notes" : "One of my favourite things to draw are flowers because every fold creates so many dynamic shades of red in a rose."
+// 		}, 
+// 	"pinball" : {
+// 		"title" : "Pinball Party",
+// 		"medium" : "clay, pencil crayon, construction paper, wood, paint",
+// 		"inspiration" : "Charles Bell - an incredible still life painter with extreme care for detail and love for bright colours.",
+// 		"notes" : "The background of the board contains facts about Charles Bell. I always tried to add a little more in my art pieces made in school. The addition of wood was unique amonst my classmates; they stuck to using only clay. Believe me when I say I was extremely excited to see this design come to life."
+// 		},
+// 	"child" : {
+// 		"title" : "Childhood",
+// 		"medium" : "pencil crayons, oil pastel, charcoal",
+// 		"inspiration" : "The impending evil that seems to come with life",
+// 		"notes" : "It is peculiar how joyful and openminded children are when they are young but as we grow, we seem to lose that. That is what this drawing aims to capture: the brightness and optimism of children being corrupted. Not meant to be pessimistic but it is how life seems to be. That is why we should all tap into our inner child and see the world from their point of view from time to time."
+// 		}
+// }]);
+// 	}
+// };
 
 function textdisplay(arr) {
 	var out = "";
 	var i;
 	for(i = 0; i < arr.length; i++) {
-		out += arr[i].title + arr[i].medium;
+		out += arr[i].title + arr[i].medium + '<br>';
 	}
 	document.getElementById("desc").innerHTML = out;
 }
