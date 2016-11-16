074b916c767f17c707ac3f0f9ef8da8b53c860bd
Iris Shen <ishen.zy@gmail.com>
2016-11-15 23:26:37 -0500

    JSON experiments

--- Javascript/test.js
+++ Javascript/test.js
@@ -27,3 +27,26 @@ textdisplay([
 	"medium" : "Oil Pastel"
 }
 ]); 
+
+var xmlhttp = new XMLHttpRequest();
+var url = "text.txt";
+
+xmlhttp.onreadystatechange = function() {
+	if (this.readyState == 4 && this.status == 200) {
+		var myArr = JSON.parse(this.responseText);
+		textdisplay(descstorage);
+	}
+};
+xmlhttp.open("GET", url, true);
+xmlhttp.send();
+
+// textdisplay(descstorage);
+
+function textdisplay(arr) {
+	var out = "";
+	var i;
+	for(i = 0; i < arr.length; i++) {
+		out += arr[i].title + arr[i].medium;
+	}
+	document.getElementById("desc").innerHTML = out;
+}
