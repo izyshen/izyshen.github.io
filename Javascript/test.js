83d71f123987721aba0ac204d77bd980eb310883
Iris Shen <ishen.zy@gmail.com>
2016-11-15 23:19:01 -0500

    JSON experiments

---
+++ Javascript/test.js
@@ -0,0 +1,29 @@
+// var descstorage = [
+// {
+// 	"title" : "Flowers",
+// 	"medium" : "Watercolour"
+// }, 
+// {
+// 	"title" : "Pinball", 
+// 	"medium" : "Wood, clay, paint, construction paper"
+// }, 
+// {
+// 	"title" : "Childhood", 
+// 	"medium" : "Oil Pastel"
+// }
+// ]; 
+
+textdisplay([
+{
+	"title" : "Flowers",
+	"medium" : "Watercolour"
+}, 
+{
+	"title" : "Pinball", 
+	"medium" : "Wood, clay, paint, construction paper"
+}, 
+{
+	"title" : "Childhood", 
+	"medium" : "Oil Pastel"
+}
+]); 
