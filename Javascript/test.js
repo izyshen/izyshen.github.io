faf3aad5ab7b0b7d1196b97df84a385a635103bb
Iris Shen <ishen.zy@gmail.com>
2016-11-18 00:57:21 -0500

    JSON experimentation

--- Javascript/test.js
+++ Javascript/test.js
@@ -1,5 +1,5 @@
 var xmlhttp = new XMLHttpRequest();
-var url = "text.txt";
+// var url = "text.txt";
 
 xmlhttp.onreadystatechange = function() {
 	if (this.readyState == 4 && this.status == 200) {
@@ -7,22 +7,55 @@ xmlhttp.onreadystatechange = function() {
 		textdisplay(myArr);
 	}
 };
-xmlhttp.open("GET", url, true);
-xmlhttp.send();
-		textdisplay([
-{
-	"title" : "Flowers",
-	"medium" : "Watercolour"
-}, 
-{
-	"title" : "Pinball", 
-	"medium" : "Wood, clay, paint, construction paper"
-}, 
-{
-	"title" : "Childhood", 
-	"medium" : "Oil Pastel"
+// xmlhttp.open("GET", url, true);
+// xmlhttp.send();
+		textdisplay([{
+	"general" : { 
+		"title" : "Title: ",
+		"focus" : "Focus: ",
+		"medium" : "Medium: ",
+		"inspiration" : "Inspiration: ",
+		"notes" : "Notes from the Author: "
+		},
+	"flower" : {
+		"title" : "Bouquet of Joy",
+		"focus" : "the details on flowers",
+		"medium" : "watercolour",
+		"notes" : "One of my favourite things to draw are flowers because every fold creates so many dynamic shades of red in a rose."
+		}, 
+	"pinball" : {
+		"title" : "Pinball Party",
+		"medium" : "clay, pencil crayon, construction paper, wood, paint",
+		"inspiration" : "Charles Bell - an incredible still life painter with extreme care for detail and love for bright colours.",
+		"notes" : "The background of the board contains facts about Charles Bell. I always tried to add a little more in my art pieces made in school. The addition of wood was unique amonst my classmates; they stuck to using only clay. Believe me when I say I was extremely excited to see this design come to life."
+		},
+	"child" : {
+		"title" : "Childhood",
+		"medium" : "pencil crayons, oil pastel, charcoal",
+		"inspiration" : "The impending evil that seems to come with life",
+		"notes" : "It is peculiar how joyful and openminded children are when they are young but as we grow, we seem to lose that. That is what this drawing aims to capture: the brightness and optimism of children being corrupted. Not meant to be pessimistic but it is how life seems to be. That is why we should all tap into our inner child and see the world from their point of view from time to time."
+		}
 }
 ]); 
+
+function textdisplay(arr) {
+	var out = "";
+	var i;
+	for(i = 0; i < arr.length; i++) {
+		out += arr[i].general.title + arr[i].flower.title + '<br>' + 
+		arr[i].general.medium + arr[i].flower.medium + '<br>';
+	}
+	document.getElementById("desc").innerHTML = out;}
+	// document.getElementById("desc").innerHTML = 
+	// 	myArr.general.title + "<br>" +
+	// 	myArr.flower.title+ "<br>"; 
+
+
+
+
+
+
+
 // var description = '{
 // 	"general" : { 
 // 		"title" : "Title: ",
@@ -50,14 +83,31 @@ xmlhttp.send();
 // 		"notes" : "It is peculiar how joyful and openminded children are when they are young but as we grow, we seem to lose that. That is what this drawing aims to capture: the brightness and optimism of children being corrupted. Not meant to be pessimistic but it is how life seems to be. That is why we should all tap into our inner child and see the world from their point of view from time to time."
 // 		}
 // }'
-function textdisplay(arr) {
-	var out = "";
-	var i;
-	for(i = 0; i < arr.length; i++) {
-		out += arr[i].title + '<br>' + 
-		arr[i].medium + '<br>';
-	}
-	document.getElementById("desc").innerHTML = out;}
-	// document.getElementById("desc").innerHTML = 
-	// 	myArr.general.title + "<br>" +
-	// 	myArr.flower.title+ "<br>"; 
+
+
+// 		textdisplay([
+// {
+// 	"title" : "Flowers",
+// 	"medium" : "Watercolour"
+// }, 
+// {
+// 	"title" : "Pinball", 
+// 	"medium" : "Wood, clay, paint, construction paper"
+// }, 
+// {
+// 	"title" : "Childhood", 
+// 	"medium" : "Oil Pastel"
+// }
+// ]); 
+
+// 		function textdisplay(arr) {
+// 	var out = "";
+// 	var i;
+// 	for(i = 0; i < arr.length; i++) {
+// 		out += arr[i].title + '<br>' + 
+// 		arr[i].medium + '<br>';
+// 	}
+// 	document.getElementById("desc").innerHTML = out;}
+// 	// document.getElementById("desc").innerHTML = 
+// 	// 	myArr.general.title + "<br>" +
+// 	// 	myArr.flower.title+ "<br>"; 
