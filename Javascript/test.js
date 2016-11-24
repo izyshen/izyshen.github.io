ee572b849a97c340e91ea598350458f86ad94147
Iris Shen <ishen.zy@gmail.com>
2016-11-23 23:57:39 -0500

    lightbox alteration

---
+++ Javascript/test.js
@@ -0,0 +1,38 @@
+jQuery(document).ready(function($) {
+	$('.lightbox_trigger').click(function(e) {
+		e.preventDefault();
+		var image_href = $(this).attr("href");
+		if ($('#lightbox').length > 0) { // #lightbox exists
+			//insert img tag with clicked link's href as src value
+			$('#content').html('<img src="'+ image_href + '" />');
+		   	
+			//show lightbox window - you can use a transition here if you want, i.e. .show('fast')
+			$('#lightbox').show();
+		}
+		else { //#lightbox does not exist 
+
+			//create HTML markup for lightbox window
+			var lightbox = 
+			'<div id="lightbox">' +
+				'<p id=nature-description>I feel most at peace when surrounded by nature.</p>' +
+				'<div id="content">' + //insert clicked link's href into img src
+					'<img src="' + image_href +'" />' +
+				'</div>' +	
+			'</div>';
+				
+			//insert lightbox HTML into page
+			$('body').append(lightbox);
+		}
+	});
+		$('#lightbox').live('click', function() {
+			$('#lightbox').hide();
+	});
+});
+
+var items = ["fish", "dog", "cat", "tom"];
+var text = "";
+var i;
+for(i = 0; i < items.length; i++) {
+    text += items[i] + "<br>";
+}
+document.getElementById("test").innerHTML = text;
