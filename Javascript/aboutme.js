c20050282cdc22b6b0360794571e0b24aceb7fb2
Iris Shen <ishen.zy@gmail.com>
2017-01-25 18:56:06 -0500

    Another attempt at fixing lightboxes

--- Javascript/aboutme.js
+++ Javascript/aboutme.js
@@ -14,7 +14,7 @@ jQuery(document).ready(function($) {
 			//create HTML markup for lightbox window
 			var lightbox = 
 			'<div id="lightbox">' +
-				'<p id=nature-description>I feel most at peace when surrounded by nature.</p>' +
+				'<p>caption</p>' +
 				'<div id="content">' + //insert clicked link's href into img src
 					'<img src="' + image_href +'" />' +
 				'</div>' +	
@@ -29,6 +29,42 @@ jQuery(document).ready(function($) {
 	});
 });
 
+var textlist = ["caption1", "caption2", "caption3", "caption4", "caption5", "caption6"]
+jQuery(document).ready(function($) {
+	$('.lightbox_trigger2').click(function(e) {
+		e.preventDefault();
+		var image_href = $(this).attr("href");
+		if ($('#lightbox').length > 0) { // #lightbox exists
+			//insert img tag with clicked link's href as src value
+			$('#content').html('<img src="'+ image_href + '" />');
+		   	
+			//show lightbox window - you can use a transition here if you want, i.e. .show('fast')
+			$('#lightbox').show();
+		}
+		else { //#lightbox does not exist 
+
+			//create HTML markup for lightbox window
+																								caption = for (i = 0; i < captions.length; i++) {
+																									textlist[i];
+																								}
+			var lightbox = 
+			'<div id="lightbox">' +
+				'<p id=nature-description>'+ caption +'</p>' +
+				'<div id="content">' + //insert clicked link's href into img src
+					'<img src="' + image_href +'" />' +
+				'</div>' +	
+			'</div>';
+				
+			//insert lightbox HTML into page
+			$('body').append(lightbox);
+		}
+	});
+		$('#lightbox').live('click', function() {
+			$('#lightbox').hide();
+	});
+});
+
+
 var items = ["fish", "dog", "cat", "tom"];
 var text = "";
 var i;
@@ -36,3 +72,4 @@ for(i = 0; i < items.length; i++) {
     text += items[i] + "<br>";
 }
 document.getElementById("test").innerHTML = text;
+
